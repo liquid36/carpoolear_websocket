fade5bbb484402643c78febdf22f8aadb76a252c
Mariano Botta <marianoabotta@gmail.com>
2018-03-22 18:05:34 -0300

    By pass messages

--- index.js
+++ index.js
@@ -3,35 +3,44 @@ let redis = require('redis');
 
 let server = require('http').createServer();
 let io = require('socket.io')(server);
+server.listen(8890); 
+
 
 let secret = 'GNU0yagwZuyueXKV0OSfyNXLIqk9bpeR';
 
 // [TODO] ENV VAR
 
-let redisClient = redis.createClient(6379, '172.19.0.4'); 
-
-server.listen(8890); 
-
+let redisClient = redis.createClient(6379, '172.19.0.3'); 
 redisClient.subscribe('user-notify'); 
- 
-redisClient.on("message", function(channel, message) {
-    console.log("mew message in queue "+ message + " channel " + channel);
-    io.emit(channel, message);
+
+redisClient.on("message", function(channel, plainText) {
+    console.log(channel);
+    let message = JSON.parse(plainText);
+    let payload = message.data;
+    switch (channel) {
+        case 'user-notify':
+            io.to(`user-${payload.user.id}`).emit(message.event, payload);
+            break;
+    }
+    // console.log("mew message in queue "+ message + " channel " + channel);
+    
 });
 
 io.on('connection', function (socket) {
  
     console.log("new client connected");
  
-    socket.on('auth', function (data) {
-        jwt.verify(data, secret, function (err, encode) {
-            console.log(err, encode);
+    socket.on('auth', function (token) {
+        jwt.verify(token, secret, function (err, encode) {
             if (err) {
                 socket.emit('auth', {status: false});
             } else {
+                let userId = encode.sub;
+                socket.join(`user-${userId}`);
                 socket.emit('auth', {status: true});
+                console.log(`user-${userId} Auth`);
             }
-        });
+        }); 
     });
  
     socket.on('disconnect', function() {
