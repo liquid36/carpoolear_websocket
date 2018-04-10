98c771e14faf9710fd6020a9c52f33c23a98a819
Mariano Botta <marianoabotta@gmail.com>
2018-04-10 17:57:31 -0300

    Websockts

---
+++ clases.js
@@ -0,0 +1,10 @@
+class WsSendMessage {
+    constructor(to, message) {
+        this.to = to;
+        this.message = message;
+    }
+}
+
+export {
+    WsSendMessage
+};
--- index.js
+++ index.js
@@ -1,33 +1,45 @@
+import { WsSendMessage } from './classes.js' 
+
 
 require('dotenv').config({path: '../backend/.env'});
 
-let jwt = require('jsonwebtoken');
+
+
 let redis = require('redis');
+let secret = process.env.JWT_SECRET;
+let redisPort = process.env.REDIS_PORT || '6379';
+let redisHost = process.env.REDIS_HOST || '172.19.0.4';
+let redisClient = redis.createClient(redisPort, redisHost); 
+
+let Noderavel = require('@movilizame/noderavel');
+
+
+
+let queueWorker = new Noderavel({
+    client: redisClient,
+    driver: 'redis',
+    scope: {
+        'STS\\Jobs\\WsSendMessage': WsSendMessage
+    }
+});
+
+queueWorker.on('job', ({name, data}) => {
+    switch (name) {
+        case 'STS\\Jobs\\WsSendMessage':
+            let channel = `user-${data.to}`;
+            let payload = data.message;
+            io.to(channel).emit('new-message', payload);
+            break;
+    } 
+});
+
+queueWorker.listen();
+
+let jwt = require('jsonwebtoken');
 
 let server = require('http').createServer();
 let io = require('socket.io')(server);
 server.listen(8890);
-
-
-let secret = process.env.JWT_SECRET;
-let redisPort = process.env.REDIS_PORT || '6379';
-let redisHost = process.env.REDIS_HOST || '172.19.0.4';
-// [TODO] ENV VAR
-
-let redisClient = redis.createClient(redisPort, redisHost); 
-
-redisClient.on('ready', () => {
-    console.log('Ready Events');
-    redisClient.psubscribe('user-*'); 
-});
-
-redisClient.on('pmessage', function(pattern, channel, plainText) {
-    console.log(channel, plainText);
-    let message = JSON.parse(plainText);
-    let payload = message.data;
-    io.to(channel).emit(message.event, payload);
-    
-});
 
 io.on('connection', function (socket) {
  
